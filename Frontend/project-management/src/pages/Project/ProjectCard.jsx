f30fca34118b880d807f2ca8f0b6041b4ef83b87
Lakshani Galwatta <nilu.galwatta@gmail.com>
2024-07-18 18:11:45 +0530

    Added Project Card

--- Frontend/project-management/src/pages/Project/ProjectCard.jsx
+++ Frontend/project-management/src/pages/Project/ProjectCard.jsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import { Card, CardContent } from "@/components/ui/card";
+import React from "react";
+import { Card } from "@/components/ui/card";
+import { DotFilledIcon, DotsVerticalIcon } from "@radix-ui/react-icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge"
+
 
 const ProjectCard = () => {
   return (
     <Card className="p-5 w-full lg:max-w-3xl">
-       Project Card 
-    </Card>
-  )
-}
+      <div className="space-y-5">
+        <div className="space-y-2">
+          <div className="flex justify-between">
+            <div className="flex items-center gap-5">
+              <h1 className="cursoe-pointer font-bold text-lg">
+                Create Ecommerce Project
+              </h1>
+              <DotFilledIcon />
+              <p className="text-sm txtgr">fullstack</p>
+            </div>
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button className="rounded-full" variant="ghost" size="icon">
+                    <DotsVerticalIcon></DotsVerticalIcon>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>Update</DropdownMenuItem>
+                  <DropdownMenuItem>Delete</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+          <p className="text-gray-500 text-sm">
+            This is a fullstack project. 
+          </p>
+        </div>
 
-export default ProjectCard
+        <div className="flex flex-wrap gap-2 items-center">
+           {
+            [1,1,1,1,].map((item)=><Badge key={item} variant="outline">javascript</Badge>)
+           }
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default ProjectCard;
