d79e5b20c1c9b2855264c7ebc67eb835cf27131f
Lakshani Galwatta <nilu.galwatta@gmail.com>
2024-07-18 17:55:47 +0530

    Added projectList

--- Frontend/project-management/src/App.jsx
+++ Frontend/project-management/src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from "@/components/ui/button"
-import './App.css'
-
+import "./App.css";
+import Home from "./pages/Home/Home";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <Button >
-          count is 
-        </Button>
-        
-    </> 
-  )
+      <Home />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/project-management/src/pages/Project/ProjectCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, CardContent } from "@/components/ui/card";
+
+const ProjectCard = () => {
+  return (
+    <Card className="p-5 w-full lg:max-w-3xl">
+       Project Card 
+    </Card>
+  )
+}
+
+export default ProjectCard
