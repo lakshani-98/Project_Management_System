73fef2fa37ba7e44faaef0ae82e545c454cefad3
Lakshani Galwatta <nilu.galwatta@gmail.com>
2024-08-01 14:28:48 +0530

    Added Project Service

---
+++ backend/projectmanagementsystem/src/main/java/com/lakshani/projectmanagementsystem/service/ProjectService.java
@@ -0,0 +1,27 @@
+package com.lakshani.projectmanagementsystem.service;
+
+import com.lakshani.projectmanagementsystem.model.Project;
+import com.lakshani.projectmanagementsystem.model.User;
+import com.lakshani.projectmanagementsystem.model.Chat;
+import java.util.List;
+
+
+public interface ProjectService {
+    Project createProject(Project project, User user) throws Exception;
+
+
+    List<Project> getProjectByTeam(User user, String category, String tag) throws Exception;
+
+    Project getProjectbyId(Long projectId)throws Exception;
+
+    void deleteProject(Long projectId, Long userId)throws Exception;
+
+    Project updateProject(Project updatedProject, Long id)throws Exception;
+
+    void addUserToProject(Long projectId, Long userId)throws Exception;
+
+    void removeUserFromProject(Long projectId, Long userId)throws Exception;
+
+    Chat getChatByProjectId(Long projectId)throws Exception;
+
+}
