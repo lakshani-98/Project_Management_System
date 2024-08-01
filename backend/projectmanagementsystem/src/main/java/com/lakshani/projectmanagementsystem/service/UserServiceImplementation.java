e790594249da2564fbfcbb1a78269bf057c6818b
Lakshani Galwatta <nilu.galwatta@gmail.com>
2024-08-01 14:51:55 +0530

    Added User Service Implementation

--- backend/projectmanagementsystem/src/main/java/com/lakshani/projectmanagementsystem/service/UserServiceImplementation.java
+++ backend/projectmanagementsystem/src/main/java/com/lakshani/projectmanagementsystem/service/UserServiceImplementation.java
@@ -1,25 +1,49 @@
 package com.lakshani.projectmanagementsystem.service;
 
+import com.lakshani.projectmanagementsystem.config.JwtProvider;
 import com.lakshani.projectmanagementsystem.model.User;
+import com.lakshani.projectmanagementsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class UserServiceImplementation implements UserService{
+import java.util.Optional;
+
+public class UserServiceImplementation implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public User findUserProfileByJwt(String jwt) throws Exception {
-        return null;
+        String email = JwtProvider.getEmailFromToken(jwt);
+
+        return findUserByEmail(email);
     }
 
     @Override
     public User findUserByEmail(String email) throws Exception {
-        return null;
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new Exception("user not found");
+        }
+        return user;
     }
+
 
     @Override
     public User findUserById(Long userId) throws Exception {
-        return null;
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isEmpty()) {
+            throw new Exception("user not found");
+        }
+
+        return optionalUser.get();
     }
 
     @Override
     public User updateUsersProjectSize(User user, int number) {
-        return null;
+        user.setProjectSize(user.getProjectSize() + number);
+
+        return userRepository.save(user);
     }
+
 }
+
