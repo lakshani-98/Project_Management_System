fa0137520db37aa8a5df9f775ae1f4a3bbfee798
Lakshani Galwatta <nilu.galwatta@gmail.com>
2024-08-16 05:24:54 +0530

    Added login and sign up implementation

--- Frontend/project-management/src/App.jsx
+++ Frontend/project-management/src/App.jsx
@@ -4,16 +4,23 @@ import NavBar from "./pages/Navbar/NavBar";
 import { Routes, Route } from "react-router-dom";
 import ProjectDetails from "./pages/ProjectDetails/ProjectDetails";
 import IssueDetails from "./pages/IssueDetails/IssueDetails";
+import Auth from "./pages/Auth/Auth";
 
 function App() {
   return (
     <>
+    {
+      false? 
+      <div>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/project/:id" element={<ProjectDetails />} />
-        <Route path="/project/:projectId/issue/:issueId" element={<IssueDetails/>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project/:id" element={<ProjectDetails />} />
+          <Route path="/project/:projectId/issue/:issueId" element={<IssueDetails/>} />
+        </Routes>
+      </div>:<Auth/>
+    }
+      
     </>
   );
 }
