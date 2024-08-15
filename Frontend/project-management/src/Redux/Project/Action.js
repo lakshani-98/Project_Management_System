d5422d270ac2104fb7c5e5ec7619c107a94166bc
Lakshani Galwatta <nilu.galwatta@gmail.com>
2024-08-15 13:20:56 +0530

    Added changes on project

---
+++ Frontend/project-management/src/Redux/Project/Action.js
@@ -0,0 +1,103 @@
+import api from "@/config/api";
+import { FETCH_PROJECTS_REQUEST, FETCH_PROJECTS_SUCCESS } from "./ActionTypes";
+import {
+  FETCH_PROJECT_BY_ID_REQUEST,
+  FETCH_PROJECT_BY_ID_SUCCESS,
+} from "./ActionType";
+
+export const fetchProjects =
+  ({ category, tag }) =>
+  async (dispatch) => {
+    dispatch({ type: FETCH_PROJECTS_REQUEST });
+    try {
+      const { data } = await api.get("/api/projects", {
+        params: { category, tag },
+      });
+      console.log("all projects", data);
+      dispatch({ type: FETCH_PROJECTS_SUCCESS, projects: data });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+export const searchProjects = (keyword) => async (dispatch) => {
+  dispatch({ type: SEARCH_PROJECT_REQUEST });
+  try {
+    const { data } = await api.get("/api/projects/search?keyword=" + keyword);
+    console.log("search projects", data);
+    dispatch({ type: SEARCH_PROJECT_SUCCESS, projects: data });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const createProject = (projectData) => async (dispatch) => {
+  dispatch({ type: CREATE_PROJECT_REQUEST });
+  try {
+    const { data } = await api.post("/api/projects", projectData);
+    console.log("create project", data);
+    dispatch({ type: CREATE_PROJECT_SUCCESS, project: data });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const fetchProjectById = (id) => async (dispatch) => {
+  dispatch({ type: FETCH_PROJECT_BY_ID_REQUEST });
+  try {
+    const { data } = await api.get("/api/projects" + id);
+    console.log("project", data);
+    dispatch({ type: FETCH_PROJECT_BY_ID_SUCCESS, project: data });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const deleteProject =
+  ({ projectId }) =>
+  async (dispatch) => {
+    dispatch({ type: DELETE_PROJECT_REQUEST });
+    try {
+      const { data } = await api.delete("/api/projects" + projectId);
+      console.log("delete projects", data);
+      dispatch({ type: DELETE_PROJECT_SUCCESS, projectId });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+export const inviteToProject =
+  ({ email, projectId }) =>
+  async (dispatch) => {
+    dispatch({ type: INVITE_TO_PROJECT_REQUEST });
+    try {
+      const { data } = await api.post("/api/projects/invite", {
+        email,
+        projectId,
+      });
+      console.log("invite projects", data);
+      dispatch({ type: INVITE_TO_PROJECT_SUCCESS, payload: data });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+export const acceptInvitation =
+  ({ invitationToken, navigate }) =>
+  async (dispatch) => {
+    dispatch({ type: ACCEPT_INVITATION_REQUEST });
+    try {
+      const { data } = await api.get("/api/projects/accept_invitation", {
+        params: {
+          token: invitationToken,
+        },
+      });
+
+      navigate("/project" + data.projectId);
+
+      console.log("invite projects", data);
+      dispatch({ type: ACCEPT_INVITATION_SUCCESS, payload: data });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
--- Frontend/project-management/src/Redux/Store.js
+++ Frontend/project-management/src/Redux/Store.js
@@ -1,9 +1,11 @@
 import {combineReducers, legacy_createStore} from "redux";
 import {thunk} from "redux-thunk";
 import { authReducer } from "./Auth/Reducer";
+import { projectReducer } from "./Project/Reducer";
 
 const rootReducer=combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    project: projectReducer
 })
 
 export const store=legacy_createStore(rootReducer, applyMiddleware(thunk))
