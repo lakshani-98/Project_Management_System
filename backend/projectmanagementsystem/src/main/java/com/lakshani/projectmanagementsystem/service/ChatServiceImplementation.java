47da4c55af049680702357df7557288442fa8f4c
Lakshani Galwatta <nilu.galwatta@gmail.com>
2024-08-17 14:37:08 +0530

    Bug fixed

--- backend/projectmanagementsystem/src/main/java/com/lakshani/projectmanagementsystem/service/ChatServiceImplementation.java
+++ backend/projectmanagementsystem/src/main/java/com/lakshani/projectmanagementsystem/service/ChatServiceImplementation.java
@@ -2,15 +2,18 @@ package com.lakshani.projectmanagementsystem.service;
 
 import com.lakshani.projectmanagementsystem.model.Chat;
 import com.lakshani.projectmanagementsystem.repository.ChatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatServiceImplementation implements ChatService {
 
+    @Autowired
     private ChatRepository chatRepository;
     @Override
     public Chat createChat(Chat chat) {
         return chatRepository.save(chat);
     }
 }
+
 
